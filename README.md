50c01bf22cc8bdd1ff6ebef546b07afc05ce8c2e
Terrence Wong <wongterrencew@gmail.com>
2017-06-20 23:37:01 +0200

    Add example of setting up jest-styled-components

---
+++ README.md
@@ -0,0 +1,6 @@
+# jest-styled-components-example
+
+An example of how to setup [`jest-styled-components`](https://github.com/styled-components/jest-styled-components)
+
+
+See `Storyshots.test.js`
---
+++ stories/Button.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
+import PropTypes from 'prop-types'
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  border: 1px solid #eee;
+  borderRadius: 3;
+  backgroundColor: #FFFFFF;
+  cursor: pointer;
+  fontSize: 15;
+  padding: 3px 10px;
+  margin: 10;
+`
+
+export default Button
